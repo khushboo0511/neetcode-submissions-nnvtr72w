878331e9641b41673564d623dd2357e2d0330494
Khushboo Chaudhary <69863418+khushboo0511@users.noreply.github.com>
2026-04-14 17:56:35 +0530

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        StringBuilder cleaned = new StringBuilder();
+
+        for (char c : s.toCharArray()) {
+            if (Character.isLetterOrDigit(c)) {
+                cleaned.append(Character.toLowerCase(c));
+            }
+        }
+
+        String str = cleaned.toString();
+        String reversed = cleaned.reverse().toString();
+
+        return str.equals(reversed);
+    }
+}
